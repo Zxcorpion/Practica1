8fe6fe3082cf98c80af5908e04283ed1fb0dd396
Marcofati <mdv00011@red.ujaen.es>
2025-09-29 17:12:50 +0200

    Update PaMedicamento.cpp

--- PaMedicamento.cpp
+++ PaMedicamento.cpp
@@ -1,6 +1,12 @@
 
 
 #include "PaMedicamento.h"
+/**
+ * @brief Constructor por defecto y parametrizado
+ * @param int id_num numero de identificación de medicamento que por defecto es 0
+ * @param string id_alpha numero otra forma de identificación del medicamento que por defecto es " "
+ * @param string nombre_ numero nombre del medicamento que por defecto es " "
+ */
 PaMedicamento::PaMedicamento(int id_num_,std::string id_alpha_, std::string nombre_):
 id_num(id_num_),
 id_alpha(id_alpha_),
@@ -8,17 +14,27 @@ nombre(nombre_)
 
 {}
 
-
+/**
+ * @brief Constructor de copia
+ * @param orig Objeto que se quiere copiar
+ * @post Se crea un nuevo objeto de clase PaMedicamento con los atributos de orig
+ */
 PaMedicamento::PaMedicamento(const PaMedicamento &orig):
 id_num(orig.id_num),
 id_alpha(orig.id_alpha),
 nombre(orig.nombre)
 {}
-
+/**
+ * @brief Destructor de la clase PaMedicamento
+ */
 PaMedicamento::~PaMedicamento() {}
 
 
-
+/**
+ * @brief Constructor de copia
+ * @param orig Objeto que se quiere copiar
+ * @post Se crea un nuevo objeto de clase PaMedicamento con los atributos de orig
+ */
 int PaMedicamento::get_id_num() const {
     return id_num;
 }
@@ -59,4 +75,5 @@ PaMedicamento &PaMedicamento::operator=(const PaMedicamento &orig) {
 bool PaMedicamento::operator==(const PaMedicamento &orig) const {
     return orig.id_num == this->id_num;
 }
+
 
